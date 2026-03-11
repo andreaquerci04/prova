77641f4428c8eefb5e786d0a14daeb585cbbd736
andreaquerci04 <andrea.querci04@hmail.com>
2026-03-11 12:41:19 +0000

    esercitazione_1

---
+++ esercitazione1/build/CMakeFiles/programma.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programma.dir/link.d"
+  "CMakeFiles/programma.dir/programma.cpp.o"
+  "CMakeFiles/programma.dir/programma.cpp.o.d"
+  "programma"
+  "programma.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programma.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
